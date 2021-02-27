d173946da44334d960851a01658597ba1d26d5bd
Tetiana-Kor <63547594+Tetiana-Kor@users.noreply.github.com>
2021-02-27 23:26:51 +0200

    add mongoose

--- model/index.js
+++ model/index.js
@@ -1,53 +1,32 @@
-const db = require("./db");
-const { ObjectID } = require("mongodb");
-
-const getCollection = async (db, name) => {
-  const client = await db;
-  const collection = await client.db().collection(name);
-  return collection;
-};
+const Contact = require("./schemas/contact");
 
 const listContacts = async () => {
-  const collection = await getCollection(db, "contacts");
-  const results = await collection.find({}).toArray();
+  const results = await Contact.find({});
   return results;
 };
 
 const getContactById = async (id) => {
-  const collection = await getCollection(db, "contacts");
-  const objectId = new ObjectID(id);
-  // console.log(objectId.getTimestamp());
-  const result = await collection.find({ _id: objectId }).toArray();
+  const result = await Contact.findOne({ _id: id });
   return result;
 };
 
 const addContact = async (body) => {
-  const item = { ...body };
-
-  const collection = await getCollection(db, "contacts");
-  const {
-    ops: [result],
-  } = await collection.insertOne(item);
-
+  const result = await Contact.create(body);
   return result;
 };
 
 const updateContact = async (id, body) => {
-  const collection = await getCollection(db, "contacts");
-  const objectId = new ObjectID(id);
-  const { value: result } = await collection.findOneAndUpdate(
-    { _id: objectId },
-    { $set: body },
-    { returnOriginal: false }
+  const result = await Contact.findByIdAndUpdate(
+    { _id: id },
+    { ...body },
+    { new: true }
   );
   return result;
 };
 
 const removeContact = async (id) => {
-  const collection = await getCollection(db, "contacts");
-  const objectId = new ObjectID(id);
-  const { value: result } = await collection.findOneAndDelete({
-    _id: objectId,
+  const result = await Contact.findByIdAndRemove({
+    _id: id,
   });
   return result;
 };
